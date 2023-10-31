bc7a13fd0a4c11a2d75743fd84727a5b8926e6c2
miguelmarlon <marlonmmota@gmail.com>
2023-10-30 21:25:16 -0300

    funcoes atualizar e criar BD e criando novo menu

--- criando_bd.py
+++ criando_bd.py
@@ -3,50 +3,52 @@ from tratando_dados import tratando_dados
 from scraping_escanteios import Escanteios
 from scraping_gols_marcados_sofridos import Gols
 
+class criando_banco_de_dados:
+    
+    # pesquisa_gols= 'Over 1.5'
+    # pais_teste ='Italy'
+    # liga_teste= 'Serie A'
+    # escanteios_teste='7.5'
 
-# pesquisa_gols= 'Over 1.5'
-# pais_teste ='Italy'
-# liga_teste= 'Serie A'
-# escanteios_teste='7.5'
+    quantidade_gols = ['Over 1.5', 'Over 2.5', 'Over 3.5']
+    quantidade_escanteios = ['7.5','8.5','9.5','10.5', '11.5']
 
-quantidade_gols = ['Over 1.5', 'Over 2.5', 'Over 3.5']
-quantidade_escanteios = ['7.5','8.5','9.5','10.5', '11.5']
-
-list_pais = [['Finland','Finnish Veikkausliiga'],['Italy', 'Serie A'],['England', 'Premier League'],['Spain', 'La Liga'],['Germany','Bundesliga'],['France', 'Ligue 1'],
-             ['Scotland','SPL'],['Netherlands', 'Eredivisie'],['Portugal','Portugese Liga NOS'],['Turkey','Turkish Super Lig'],['Greece','Greek Super League'],['Belgium','Pro League'],['Brazil','Serie A'],
-            ['Austria','Bundesliga'],['Russia','Premier League'],['Argentina','Primera Division'],['Denmark', 'Superliga'],['Poland','Ekstraklasa'],['USA','US MLS'],
-            ['Norway','Norwegian Eliteserien'],['Sweden','Swedish Allsvenskan'],['Switzerland','Swiss Super League'],['Australia','A League'],['Japan','J League'],
-            ['China','Super League'],['Mexico','Liga MX'],['Ukraine','Premier League'],['Czechia','Czech Liga'],['Saudi Arabia','Saudi Pro League']
-            ]
-            # ['Finland','Finnish Veikkausliiga'] não tem essa liga para corners 
-list_pais_escanteios = [['Italy', 'Serie A'],['England', 'Premier League'],['Spain', 'La Liga'],['Germany','Bundesliga'],['France', 'Ligue 1'],['Scotland','SPL'],
-                        ['Netherlands', 'Eredivisie'],['Portugal','Portugese Liga NOS'],['Turkey','Turkish Super Lig'],['Greece','Greek Super League'],['Belgium','Pro League'],
-                        ['Brazil','Serie A'],['Austria','Bundesliga'],['Russia','Premier League'],['Argentina','Primera Division'],['Denmark', 'Superliga'],['Poland','Ekstraklasa'],
-                        ['USA','US MLS'],['Norway','Norwegian Eliteserien'],['Sweden','Swedish Allsvenskan'],['Switzerland','Swiss Super League'],['Australia','A League'],['Japan','J League'],
-                        ['China','Super League'],['Mexico','Liga MX'],['Ukraine','Premier League'],['Czechia','Czech Liga'],['Saudi Arabia','Saudi Pro League']]                   
-
-
-#criando bando de dados de gols
-# for gol in quantidade_gols:
-#     for pais, liga in list_pais:
-#         gols = Gols(pais, liga, gol)
-#         dados = gols.cria_bd_gols()
-#         print (dados)
+    list_pais = [['Finland','Finnish Veikkausliiga'],['Italy', 'Serie A'],['England', 'Premier League'],['Spain', 'La Liga'],['Germany','Bundesliga'],['France', 'Ligue 1'],
+                ['Scotland','SPL'],['Netherlands', 'Eredivisie'],['Portugal','Portugese Liga NOS'],['Turkey','Turkish Super Lig'],['Greece','Greek Super League'],['Belgium','Pro League'],['Brazil','Serie A'],
+                ['Austria','Bundesliga'],['Russia','Premier League'],['Argentina','Primera Division'],['Denmark', 'Superliga'],['Poland','Ekstraklasa'],['USA','US MLS'],
+                ['Norway','Norwegian Eliteserien'],['Sweden','Swedish Allsvenskan'],['Switzerland','Swiss Super League'],['Australia','A League'],['Japan','J League'],
+                ['China','Super League'],['Mexico','Liga MX'],['Ukraine','Premier League'],['Czechia','Czech Liga'],['Saudi Arabia','Saudi Pro League']
+                ]
+                # ['Finland','Finnish Veikkausliiga'] não tem essa liga para corners 
+    list_pais_escanteios = [['Italy', 'Serie A'],['England', 'Premier League'],['Spain', 'La Liga'],['Germany','Bundesliga'],['France', 'Ligue 1'],['Scotland','SPL'],
+                            ['Netherlands', 'Eredivisie'],['Portugal','Portugese Liga NOS'],['Turkey','Turkish Super Lig'],['Greece','Greek Super League'],['Belgium','Pro League'],
+                            ['Brazil','Serie A'],['Austria','Bundesliga'],['Russia','Premier League'],['Argentina','Primera Division'],['Denmark', 'Superliga'],['Poland','Ekstraklasa'],
+                            ['USA','US MLS'],['Norway','Norwegian Eliteserien'],['Sweden','Swedish Allsvenskan'],['Switzerland','Swiss Super League'],['Australia','A League'],['Japan','J League'],
+                            ['China','Super League'],['Mexico','Liga MX'],['Ukraine','Premier League'],['Czechia','Czech Liga'],['Saudi Arabia','Saudi Pro League']]                   
 
 
-# criando estatísticas de escanteios no DB
-# for escanteio in quantidade_escanteios:
-#     for pais, liga in list_pais_escanteios:
-     
-#         escanteios = Escanteios(pais, liga, escanteio)
-#         dados = escanteios.cria_bd_escanteios()
-#         print (dados)
+    #criando bando de dados de gols
+    def criando_banco_de_dados_gols(self):
+        for gol in self.quantidade_gols:
+            for pais, liga in self.list_pais:
+                gols = Gols(pais, liga, gol)
+                dados = gols.cria_bd_gols()
+                print (dados)
 
-#atualizando bando de dados de gols
-for gol in quantidade_gols:
-    for pais, liga in list_pais:
-        gols = Gols(pais, liga, gol)
-        dados = gols.atualiza_bd_gols()
-        print (dados)
 
-a=1
+    # criando estatísticas de escanteios no DB
+    def criando_banco_de_dados_escanteios(self):
+        for escanteio in self.quantidade_escanteios:
+            for pais, liga in self.list_pais_escanteios:
+                escanteios = Escanteios(pais, liga, escanteio)
+                dados = escanteios.cria_bd_escanteios()
+                print (dados)
+
+    #atualizando bando de dados de gols
+    def atualizando_banco_de_dados_gols(self):
+        for gol in self.quantidade_gols:
+            for pais, liga in self.list_pais:
+                gols = Gols(pais, liga, gol)
+                dados = gols.atualiza_bd_gols()
+                print (dados)
+        
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 from criando_conexao_bd import conexao_bd
+from criando_bd import criando_banco_de_dados
 
 load_dotenv()
-
 conexao = conexao_bd.conectando()
 lista_times= []
 
@@ -14,41 +14,56 @@ print('******************BEM VINDO AO PROGRAMA!******************')
 print()
 
 while True:
-    time_casa = input('DIGITE O NOME DO TIME DA CASA:').capitalize()
-    cursor = conexao.cursor()
-    consulta_time_casa = "SELECT * FROM gols WHERE nome LIKE %s"
-    cursor.execute(consulta_time_casa, (f'%{time_casa}%',))
-    resultado_casa = cursor.fetchall()
+    opcao = input('0 - SAIR\n\n'
+                '1 - ENTRAR NA SELEÇÃO DE TIMES\n\n'
+                '2 - ATUALIZAR O BANCO DE DADOS\n\n'
+                '3 - CRIAR O BANCO DE DADOS\n\n'
+                'OPÇÃO : ')
+    match opcao:
+        case '0':
+            print('SAINDO DO PROGRAMA...')
+            exit()
+        case '1':
+            while True:
+                time_casa = input('DIGITE O NOME DO TIME DA CASA:').capitalize()
+                cursor = conexao.cursor()
+                consulta_time_casa = "SELECT * FROM gols WHERE nome LIKE %s"
+                cursor.execute(consulta_time_casa, (f'%{time_casa}%',))
+                resultado_casa = cursor.fetchall()
 
-    if resultado_casa:
-        time_fora = input('DIGITE O NOME DO TIME VISITANTE:').capitalize()
-        consulta_time_fora = "SELECT * FROM gols WHERE nome LIKE %s"
-        cursor.execute(consulta_time_fora, (f'%{time_fora}%',))
-        resultado_fora = cursor.fetchall()
+                if resultado_casa:
+                    time_fora = input('DIGITE O NOME DO TIME VISITANTE:').capitalize()
+                    consulta_time_fora = "SELECT * FROM gols WHERE nome LIKE %s"
+                    cursor.execute(consulta_time_fora, (f'%{time_fora}%',))
+                    resultado_fora = cursor.fetchall()
+                    
+                    if resultado_fora:
+                        lista_temporaria = [time_casa, time_fora]
+                        lista_times.append(lista_temporaria)
+                    else:
+                        print(f'O TIME {time_fora} NÃO FOI ENCONTRADO!')
+                else:
+                    print(f'O TIME {time_casa} NÃO FOI ENCONTRADO!')
+
+                continuar = input('DIGITE QUALQUER TECLA PARA CONTINUAR OU 2 PARA SAIR:')
+                if continuar == '2':
+                    print('AGUARDE UM INSTANTE PARA FINALIZAR:)')
+                    print()
+                    break
+                
+            for time_casa, time_fora in lista_times:    
+                objeto = tratando_dados(time_casa, time_fora)       
+                df_gols = objeto.estatistica_filtrada_gol()
+            conexao.commit()
+            conexao.close()
+            
+        case '2':
+            objeto = criando_banco_de_dados.atualizando_banco_de_dados_gols()
         
-        if resultado_fora:
-            lista_temporaria = [time_casa, time_fora]
-            lista_times.append(lista_temporaria)
-        else:
-            print(f'O TIME {time_fora} NÃO FOI ENCONTRADO!')
-    else:
-        print(f'O TIME {time_casa} NÃO FOI ENCONTRADO!')
-
-    continuar = input('DIGITE QUALQUER TECLA PARA CONTINUAR OU 2 PARA SAIR:')
-    if continuar == '2':
-        print('AGUARDE UM INSTANTE PARA FINALIZAR:)')
-        print()
-        break
-for time_casa, time_fora in lista_times:    
-    teste = tratando_dados(time_casa, time_fora)       
-    df_gols = teste.estatistica_filtrada_gol()
-
-conexao.commit()
-conexao.close()
-
-a=1
-
-
-
-
-
+        case '3':
+            objeto_um = criando_banco_de_dados.criando_banco_de_dados_gols()
+            objeto_dois = criando_banco_de_dados.criando_banco_de_dados_escanteios()
+        
+        case _:
+            print('OPÇÃO INVÁLIDA!')
+    break
